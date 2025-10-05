7b7a937dab15842ad9f2d8ca85d193f29663d30f
Binduvarsha23 <123391226+Binduvarsha23@users.noreply.github.com>
2025-10-05 20:33:20 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://securevaultbackend.onrender.com/api/:path*", // your backend API
+      },
+    ];
+  },
+  reactStrictMode: true,
 };
 
 export default nextConfig;
